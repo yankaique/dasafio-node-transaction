82e9f2dd7c0e6b9cabfb187ddd701f6131c33ef8
Yan <yankaiquecosta.yk@gmail.com>
2020-11-07 14:15:09 -0300

    Respositories

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,4 +1,11 @@
+/* eslint-disable no-param-reassign */
 import Transaction from '../models/Transaction';
+
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 
 interface Balance {
   income: number;
@@ -14,15 +21,42 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce((total, element): number => {
+      // eslint-disable-next-line no-return-assign
+      if (element.type === 'income') return (total += element.value);
+      return total;
+    }, 0);
+
+    const outcome = this.transactions.reduce((total, element) => {
+      // eslint-disable-next-line no-return-assign
+      if (element.type === 'outcome') return (total += element.value);
+      return total;
+    }, 0);
+
+    const balance: Balance = {
+      income,
+      outcome,
+      total: income - outcome,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const dataTransaction = new Transaction({ title, value, type });
+    const balance = this.getBalance();
+
+    if (dataTransaction.type === 'outcome' && dataTransaction.value > balance.total) {
+      throw Error("Your output value must be less than the total");
+    }
+
+    this.transactions.push(dataTransaction);
+
+    return dataTransaction;
   }
 }
 
